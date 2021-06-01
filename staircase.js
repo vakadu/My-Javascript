f13cd7bd80d9fbd6a53e2c5503413868ba1e35b4
Vakadu Vinod <vakadu@Vakadus-MacBook-Pro.local>
2021-06-01 09:20:52 +0530

    added staair case

--- staircase.js
+++ staircase.js
@@ -1,1 +1,17 @@
+// https://leetcode.com/problems/climbing-stairs/
 
+function staircase(n) {
+    if(n === 1 || n === 0) return 1;
+    let first = 1;
+    let second = 2;
+
+    for(i=3; i<=n; i++){
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    console.log(second);
+    return second;
+}
+
+staircase(5);
